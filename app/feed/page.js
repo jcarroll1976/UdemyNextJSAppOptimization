c645ec0e237fc99f4640fb8bb44d9b9a7c0fa02d
Joshua Carroll <carrolljosh1976@gmail.com>
2024-08-25 15:21:05 -0400

    modified generateMetadata function based on number of posts

--- app/feed/page.js
+++ app/feed/page.js
@@ -4,10 +4,18 @@ import { getPosts } from '@/lib/posts';
 export async function generateMetadata() {
   const posts = await getPosts();
   const numberOfPosts = posts.length;
-  return {
-    title: `Browse all ${numberOfPosts} post(s)`,
-    description: "Browse all of our posts."
+
+  if(numberOfPosts <= 1) {
+    return {
+      description: "Browse all of our posts."
+    }
+  } else {
+      return {
+        title: `Browse all ${numberOfPosts} post(s)`,
+        description: "Browse all of our posts."
+      }
   }
+
 }
 
 export default async function FeedPage() {
